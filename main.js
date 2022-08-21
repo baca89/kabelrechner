4b3c2afcda81b23aedf47253b7141f4b92230b43
Carsten Bauer <carsten.ba@t-online.de>
2022-08-21 21:14:51 +0200

    fix Klasse entfernen

--- main.js
+++ main.js
@@ -1,56 +1,6 @@
-class kabelrechner{
-    //TODO: entfernen der Klasse und umschreiben auf Funktionen
-    static kabel ={};
-    static rhoKupfer = 56;
-
-    constructor(){
-        this.kabel.laenge=0;
-        this.kabel.querschnitt=0;
-        this.kabel.strom=0;
-        this.kabel.spannungsfall=0;
-        this.kabel.spannung=0;
-        this.kabel.cosPhi=0;
-    }
-
-    /**
-     * @param {number} laenge
-     */
-    set laenge(laenge){
-        this.kabel.laenge=laenge;
-    }
-    /**
-     * @param {number} strom
-     */
-    set strom(strom){
-        this.kabel.strom=strom;
-    }
-    /**
-     * @param {number} leistung
-     */
-    set leistung(leistung){
-        this.kabel.strom = leistung/this.kabel.spannung
-    }
-    /**
-     * @param {number} spannung
-     */
-    set spannung(spannung){
-        this.kabel.spannung=spannung;
-    }
-    /**
-     * @param {number} spannungsfall
-     */
-    set spannungfall(spannungsfall){
-        this.kabel.spannungfall=this.spannungfall;
-    }
-
-
-    calcSpannungsfall() {
-        this.kabel.querschnitt=(2*this.kabel.laenge*this.kabel.strom*this.kabel.cosPhi)/(rhoKupfer*this.kabel.spannungfall*this.kabel.spannung)
-    }
-
-}
-
 const rhoKupfer = 56;
+//TODO Variable mit Normquerschnitten anlegen
+
 
 var kabel={
     laenge          :   0,
