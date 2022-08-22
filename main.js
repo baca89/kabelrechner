7817788765de29a432af2d637322982ffe6e6499
Carsten Bauer <carsten.ba@t-online.de>
2022-08-22 13:20:02 +0200

    update main.js

--- main.js
+++ main.js
@@ -14,19 +14,63 @@ var kabel={
     querschnitt     :   {
         berechnet   :   0,
         norm        :   0
-    }
+    },
+    phasen          :   0
     }
 
 
-function calcSpannungsfall(){
-    return (2*kabel.laenge*kabel.strom*kabel.cosPhi)/(rhoKupfer*kabel.spannungsfall.erlaubt*kabel.spannung);
+function calcSpannungsfall(phasen){
+    //TODO Auswahl Leitermaterial einfügen
+    let rho = rhoKupfer;
+
+    switch (phasen) {
+        case 0:
+            return 0;       //TODO Spannungsfall für Gleichstrom ergänzen
+        case 1:
+            return 2 * kabel.laenge * kabel.strom * kabel.cosPhi / rho / kabel.spannungsfall.erlaubt;
+        case 3:
+            return Math.sqrt(3) * kabel.laenge * kabel.strom * kabel.cosPhi / rho / kabel.spannungsfall.erlaubt;
+        default:
+            return 0;
+    }
 }
 
 function getNextSection(quer){
     
     for ( querschnitt in normQuerschnitte) {
         if (querschnitt>quer){
-            return querschnitt
+            return normQuerschnitte[querschnitt];
         }  
         }
     }
+
+    function calcStromFromLeistung (leistung, phasen){
+        switch (phasen) {
+            case 0:
+                return leistung / kabel.spannung;
+            case 1:
+                return leistung / kabel.spannung / kabel.cosPhi;
+            case 3:
+                return leistung / kabel.spannung / kabel.cosPhi / Math.sqrt(3);
+            default:
+                return 0;
+        }
+    }
+function setKabelWerte(){
+    //TODO Werte von Formular einlesen;
+    kabel.laenge = 65;
+    kabel.spannung = 400;
+    kabel.cosPhi = 0.8;
+    kabel.spannungsfall.erlaubt = kabel.spannung*0.03;
+    //TODO Abfrage ob Leistung oder Strom
+    kabel.strom = 35;
+    kabel.phasen = 3;
+}
+    
+
+function kabelberechnung(){
+    setKabelWerte()
+    kabel.querschnitt.berechnet=calcSpannungsfall(kabel.phasen);
+    kabel.querschnitt.norm=getNextSection(kabel.querschnitt.berechnet);
+    console.log (kabel);
+}
